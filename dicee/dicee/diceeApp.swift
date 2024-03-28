cd429ece6aa6c807e505f2805452bab8026164c4
Sarp <sarpbozk@gmail.com>
2024-03-28 14:49:31 +0300

    add assets

---
+++ dicee/dicee/diceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceeApp.swift
+//  dicee
+//
+//  Created by HOA on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct diceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
